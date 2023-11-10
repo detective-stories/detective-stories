2e53968792eb9643d85b6d77b0fd138d5e5532d7
Alexander Kovrigin <git@alexkovrigin.me>
2023-11-10 16:29:52 +0100

    try fix vercel deploy

--- dtb/settings.py
+++ dtb/settings.py
@@ -133,9 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 
 # -----> CELERY
