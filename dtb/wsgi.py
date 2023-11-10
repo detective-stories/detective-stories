3bf18b69cc197a9f266b983bbba9f83802f39cbb
Alexander Kovrigin <git@alexkovrigin.me>
2023-11-10 16:59:53 +0100

    i dunno how to fix redis, lol

--- dtb/wsgi.py
+++ dtb/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from .celery import app as celery_app
+
+__all__ = ('celery_app',)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dtb.settings')
 
 application = get_wsgi_application()
