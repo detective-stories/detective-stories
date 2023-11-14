a2210a607cf08682b833080638b94a9ae1e20bce
Alexander Kovrigin <git@alexkovrigin.me>
2023-11-14 10:57:26 +0100

    simple verdict check

--- llm_helper/chat.py
+++ llm_helper/chat.py
@@ -18,7 +18,6 @@ class LLMHelper:
         messages: List[Any],
     ) -> str:
         logger.info(f"Chat complete with messages: {messages}")
-        # TODO: Make this async (i dunno why it's not working lol)
         chat_completion = await self.client.chat.completions.create(
             messages=messages,
             model=self.model,
@@ -27,6 +26,20 @@ class LLMHelper:
         logger.debug(f"Chat complete response: {res}")
         return res
 
+    comparison_system_prompt = (
+        "Your task is to assess the semantic similarity between the player's verdict and "
+        "author's verdict. If the player's verdict is correct, answer '1'. If not, "
+        "answer '0'. The texts are separated by '###'."
+    ).strip()
+
     async def is_solved(self, player_answer: str, ground_truth: str) -> bool:
-        # TODO: Make request to OpenAI to check "text similarity"
-        return True
+        text = f"{player_answer}###{ground_truth}"
+        chat_completion = await self.client.chat.completions.create(
+            messages=[
+                {"role": "system", "content": self.comparison_system_prompt},
+                {"role": "user", "content": text},
+            ],
+            model=self.model,
+        )
+        res = chat_completion.choices[0].message.content.strip()
+        return res == "1"
